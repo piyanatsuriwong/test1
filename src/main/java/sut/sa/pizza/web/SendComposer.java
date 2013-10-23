676e6a8977ac92018e8eedd7bc79ae39366c8305
piyanatsuriwong <piyanat2533_15@hotmail.com>
2013-10-24 03:08:40 +0700

    new send.zul

---
+++ src/main/java/sut/sa/pizza/web/SendComposer.java
@@ -0,0 +1,13 @@
+package sut.sa.pizza.web;
+
+import org.zkoss.zk.roo.annotations.ZKComposer;
+import org.zkoss.zk.ui.Component;
+
+@ZKComposer(zul = "send.zul")
+public class SendComposer {
+
+    private static final long serialVersionUID = 7129373875858456862L;
+
+    public void afterCompose(Component comp) {
+    }
+}
