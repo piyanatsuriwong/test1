719a93ec77a345e70784816075a53a9a0b78cfbd
piyanatsuriwong <piyanat2533_15@hotmail.com>
2013-10-24 03:12:33 +0700

    create regit.zul

---
+++ src/main/java/sut/sa/pizza/web/RegitComposer.java
@@ -0,0 +1,13 @@
+package sut.sa.pizza.web;
+
+import org.zkoss.zk.roo.annotations.ZKComposer;
+import org.zkoss.zk.ui.Component;
+
+@ZKComposer(zul = "regit.zul")
+public class RegitComposer {
+
+    private static final long serialVersionUID = -4744125475881186294L;
+
+    public void afterCompose(Component comp) {
+    }
+}
