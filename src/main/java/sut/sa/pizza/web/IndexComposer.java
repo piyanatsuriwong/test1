801cdb384d9f582649ecbd982562e78468621613
piyanatsuriwong <piyanat2533_15@hotmail.com>
2013-10-24 03:03:06 +0700

    newproject

---
+++ src/main/java/sut/sa/pizza/web/IndexComposer.java
@@ -0,0 +1,13 @@
+package sut.sa.pizza.web;
+
+import org.zkoss.zk.roo.annotations.ZKComposer;
+import org.zkoss.zk.ui.Component;
+
+@ZKComposer(zul = "index.zul")
+public class IndexComposer {
+
+    private static final long serialVersionUID = 6203660579647856193L;
+
+    public void afterCompose(Component comp) {
+    }
+}
